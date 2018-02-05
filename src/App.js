43ad0f40b62831cf95c59a3885cc712ed0027823
james gill <infinityl00p>
2018-02-04 22:25:37 -0800

    added tilt, onclick to card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
 import React, { Component } from 'react';
 import ContractContainer from './ContractContainer';
+import ContractList from './ContractList';
+
+const data = {
+  contracts: [
+    {
+      id: 1,
+      title: "No alcohol",
+      contract: "Stringidy Stringidy Stringidy",
+      endDate: "05-19-2018",
+      witness: {
+        id: 1,
+        name: "Avneet Brar",
+        email: "avneetbrar@jizzmail.com"
+      }
+    },
+    {
+      id: 2,
+      title: "No Fap",
+      contract: "Fap Fap Fap",
+      endDate: "05-19-2018",
+      witness: {
+        id: 1,
+        name: "Avneet Brar",
+        email: "avneetbrar@jizzmail.com"
+      }
+    },
+    {
+      id: 3,
+      title: "No alcohol",
+      contract: "Stringidy Stringidy Stringidy",
+      endDate: "05-19-2018",
+      witness: {
+        id: 1,
+        name: "Avneet Brar",
+        email: "avneetbrar@jizzmail.com"
+      }
+    },
+    {
+      id: 4,
+      title: "No alcohol",
+      contract: "Stringidy Stringidy Stringidy",
+      endDate: "05-19-2018",
+      witness: {
+        id: 1,
+        name: "Avneet Brar",
+        email: "avneetbrar@jizzmail.com"
+      }
+    }
+  ]
+}
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      contracts: data.contracts
+    }
+  }
   render() {
     return (
       <div className="app container">
 
         <div className="row my-3">
           <div className="col-md-12 text-center">
-            <h1 className="title text-white">signed by me.</h1>
+            <h1 className="title text-white">signed by me</h1>
           </div>
         </div>
 
         <div className="row">
-          <div className="col-sm-12 col-lg-6">
+          <div className="col-12 col-lg-6">
             <ContractContainer />
           </div>
-          <div className="col-md-6">
-
+          <div className="col-12 mx-auto my-auto col-md-4">
+            <ContractList contracts={this.state.contracts} />
           </div>
         </div>
 
--- src/ContractContainer.js
+++ src/ContractContainer.js
@@ -1,50 +1,82 @@
 import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 class ContractContainer extends Component {
   constructor() {
     super();
+
+    this.state = {
+      startDate: moment()
+    }
+  }
+
+  handleChange = (date) => {
+    this.setState({
+      startDate: date
+    })
   }
 
   render() {
     return(
-      <div className="contract bg-white">
+      <div className="contract bg-white mb-">
 
-        <div className="col-md-12 text-center pt-2">
-          <h2>
-            <input className="contract-title text-center w-100" type="title" placeholder="Challenge Title" />
-          </h2>
-        </div>
+          <div className="col-md-12 text-center pt-2">
+            <h2>
+              <input className="light-blue text-center w-100" type="title" placeholder="Challenge Title" />
+            </h2>
+          </div>
 
-        <div className="contract-text col-md-12 p-4">
-          I
-          <input className="text-center w-40 mx-2 underline" type="name" placeholder="Your Name" />
-          agree to:
-        </div>
+          <div className="contract-text col-md-12 py-2">
+            I
+            <input className="text-center w-40 mx-2 underline" type="name" placeholder="Your Name" />
+            agree to:
+          </div>
 
-        <div className="p-3">
-          <textarea className="notes w-100" rows="6" />
-        </div>
+          <div className="p-3">
+            <textarea className="notes w-100" rows="6" />
+          </div>
 
-        <div className="contract-signature h-25">
-
-          <div className="row py-3">
-            <div className="col-md-6">
-              <input className="text-center mx-2 underline" type="name" placeholder="Your Name" />
+          <div className="row px-2 mb-2">
+            <div className="col-md-5">
+              Completion Date:
             </div>
-            <div className="col-md-6">
-              <input className="text-center mx-2 underline signature" type="name" placeholder="Signature" />
+            <div className="col-md-3 underline">
+              <DatePicker
+                selected={this.state.startDate}
+                onChange={this.handleChange}
+              />
             </div>
           </div>
 
-          <div className="row py-3">
-            <div className="col-md-6">
-              <input className="text-center mx-2 underline" type="name" placeholder="Witness Name" />
+          <div className="contract-signature mt-2 h-25">
+
+            <div className="row py-3">
+              <div className="col-md-6 col-12">
+                <input className="text-center mx-2 underline" type="name" placeholder="Your Name" />
+              </div>
+              <div className="col-md-6 col-12">
+                <input className="text-center mx-2 underline signature" type="signature" placeholder="Signature" />
+              </div>
             </div>
-            <div className="col-md-6">
-              <input className="text-center mx-2 underline" type="name" placeholder="Witness Email" />
+
+            <div className="row py-3">
+              <div className="col-md-6 col-12">
+                <input className="text-center mx-2 underline" type="witness-name" placeholder="Witness Name" />
+              </div>
+              <div className="col-md-6 col-12">
+                <input className="text-center mx-2 underline" type="witness-email" placeholder="Witness Email" />
+              </div>
             </div>
           </div>
-        </div>
+
+          <div className="row mt-2 mb-5">
+            <div className="col-md-12 text-center">
+              <button type="button" className="btn btn-outline-primary">Sign</button>
+            </div>
+          </div>
 
       </div>
     );
